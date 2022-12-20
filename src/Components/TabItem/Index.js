671ad10bf3251afa9951207dda4a589a7d63f116
isandeepkumar2000 <sandeep.kumarrrr2000@gmail.com>
2022-12-20 15:34:00 +0530

    NewChanges

---
+++ src/Components/TabItem/Index.js
@@ -0,0 +1,19 @@
+const TabItem = (props)  => {
+    const {tabDetails,clickTabItem} = props
+    const {tabId,displayText} = tabDetails
+    const onChangeTabitems = () => {
+        clickTabItem(tabId)
+    }
+    return(
+    
+    <li className="Tab_list">
+        <button type="button" className="button-items" onClick={onChangeTabitems}>
+          {displayText}
+        </button>
+    </li>
+    
+    
+    )
+    
+    }
+    export default TabItem
