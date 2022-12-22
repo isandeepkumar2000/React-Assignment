d145cd95c8a032b1e044c8b69fef8df3f3d31ba7
isandeepkumar2000 <sandeep.kumarrrr2000@gmail.com>
2022-12-22 12:52:13 +0530

    changes

--- src/Components/Appointment/Index.js
+++ src/Components/Appointment/Index.js
@@ -6,6 +6,23 @@ class Appointment extends Component {
   render() {
     return (
       <div className='Appointment_Container'>
+        <div className='Appointment_Items'>
+            <h1 className='Appointment_Heading'>Add Appointment</h1>
+            <div className='Appointment_join'>
+                <div className='Appointment_items_box'>
+                    <div>
+                        <p>TITLE</p>
+                        <input className="Name_input" placeholder="Title" />
+                    </div>
+                    <div>
+                        <p>DATE</p>
+                        
+                    </div>
+
+                </div>
+                <div className='Images_Appoinmtnent'></div>
+            </div>
+        </div>
 
       </div>
     )
